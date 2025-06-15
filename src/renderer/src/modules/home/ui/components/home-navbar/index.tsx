36b6747250b248a74cbc477d96744b5b41ec2a10
cfngc4594 <cfngc4594@gmail.com>
2025-06-15 16:15:38 +0800

    style(home-navbar): use self-closing tag for empty div

--- src/renderer/src/modules/home/ui/components/home-navbar/index.tsx
+++ src/renderer/src/modules/home/ui/components/home-navbar/index.tsx
@@ -8,8 +8,7 @@ export const HomeNavbar = () => {
         <div className="flex items-center flex-shrink-0 no-drag">
           <SidebarTrigger />
         </div>
-        <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
-        </div>
+        <div className="flex-1 flex justify-center max-w-[720px] mx-auto" />
         <div className="flex-shrink-0 items-center flex gap-4 no-drag">
           <ModeToggle />
         </div>
